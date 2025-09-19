264c89c296464ad31f570bf059991423d00d167f
TKNPLUS <tassuku.tamenari@gmail.com>
2025-09-19 14:06:23 +0900

    Fix deployment paths

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
-import { GameProvider } from './GameContext.jsx'; // ★追加
+import { GameProvider } from './GameContext.jsx';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* ▼▼▼ GameProviderで全体を囲む ▼▼▼ */}
     <GameProvider>
-        <BrowserRouter basename="/wiki-quiz">
+      {/* ↓↓↓ この行が重要です ↓↓↓ */}
+      <BrowserRouter basename="/wiki-quiz">
         <App />
       </BrowserRouter>
     </GameProvider>
-    {/* ▲▲▲ GameProviderで全体を囲む ▲▲▲ */}
   </React.StrictMode>,
 )
